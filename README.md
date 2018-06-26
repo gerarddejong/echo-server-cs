b921cbcb915db448e4e0fbe6beb5b79e62a9a208
Gerard de Jong <gerard@dejong.za.net>
2018-06-26 09:25:28 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# echo-client-cs
+Echo Server written in C# (.Net Core 2.0) for the Networking 101: TCP/IP with Wireshark Master Class series hosted at IQbusiness &amp; Girl Code.
---
+++ TCPEchoServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class TCPEchoServer
+    {
+        private const int port = 5005;
+        
+        static void Main(string[] args) 
+        {
+            try 
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+
+                while (true) 
+                { 
+                    NetworkStream networkStream = null;
+                    TcpClient client = null;
+
+                    try 
+                    {
+                        Console.WriteLine("Listening for client on port: " + port);
+
+                        client = listener.AcceptTcpClient(); // Wait for a client to connect
+                        networkStream = client.GetStream();
+                        
+                        Console.WriteLine("Accepted connection from client port: " + ((IPEndPoint)client.Client.RemoteEndPoint).Port);
+
+                        byte[] welcome = Encoding.ASCII.GetBytes("Welcome to Echo Server (type 'quit' to close connection)...\n");
+                        networkStream.Write(welcome, 0, welcome.Length);
+
+                        byte[] receiveBuffer = new byte[1024];
+                        int receivedBytesCount;
+                        while ((receivedBytesCount = networkStream.Read(receiveBuffer, 0, receiveBuffer.Length)) > 0) 
+                        {
+                            String message = Encoding.ASCII.GetString(receiveBuffer);
+                            Console.Write(message);
+                            
+                            if(message.Length > 4 && "quit".Equals(message.Substring(0, 4)))
+                            {
+                                Console.WriteLine("Client has quit!\n");
+                                break;
+                            }
+                            else
+                            {
+                                networkStream.Write(receiveBuffer, 0, receivedBytesCount); // Echo
+                            }
+                        }
+                    } 
+                    catch (Exception e) 
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    finally
+                    {
+                        networkStream.Close();
+                        client.Close();
+                    }
+                }
+            } 
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+        }
+    }
+}
